34d07e746c49a7f6b232cdfb5faee618b076e357
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2021-05-26 18:02:18 +0800

    bug

--- main/app/src/main/java/com/yfz/visualizingsortview/MainActivity.java
+++ main/app/src/main/java/com/yfz/visualizingsortview/MainActivity.java
@@ -6,18 +6,14 @@ import android.os.Bundle;
 import com.yfz.visualizingsortview.algorithm.InsertSearch;
 
 public class MainActivity extends AppCompatActivity {
-    private final int mMax = 20;
+    private final int[] array = new int[]{1,2,3,9,13,22,33,45,99,103,111,112,235,496,521,812,1002};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         InsertSearch insertSearch = new InsertSearch();
-//        int[] array = new int[mMax];
-//        for (int i = 0; i<mMax; i++){
-//            array[i]=i+1;
-//        }
-        int[] array = new int[]{1,2,3,9,13,22,45,99,103,111,112,521,812};
-        insertSearch.InsertSearch(array,0,array.length-1,3);
+        insertSearch.InsertSearch(array,0,array.length-1,125);
     }
 
 }
--- main/app/src/main/java/com/yfz/visualizingsortview/algorithm/InsertSearch.java
+++ main/app/src/main/java/com/yfz/visualizingsortview/algorithm/InsertSearch.java
@@ -6,29 +6,32 @@ public class InsertSearch {
     private String TAG = "插值查找: ";
     int interval = 0;
     //int mid = lo + (hi - lo) * (target - array[lo]) / (array[hi]-array[lo])
-    public int InsertSearch(int[] arr, int left, int right, int value){
+    public int InsertSearch(int[] array, int lo, int hi, int target){
         //注意:findVal < arr[0] 和 findVal > arr[arr.length - 1] 必须需要
         // 否则我们得到的 mid 可能越界
-        if (left > right || value < arr[0] || value > arr[arr.length - 1]) {
+        interval++;
+        if (lo > hi || target < array[0] || target > array[array.length - 1]) {
+            Log.d(TAG, "\n   第 "+interval+" 次:"+"\n 结果不存在，返回-1: ");
             return -1;
         }
         // 求出middle
-        int middle = left + (right - left) * (value - arr[left]) / (arr[right] - arr[left]);
-        if(left>middle){
+        final int mid = lo + (hi - lo) * (target - array[lo]) / (array[hi] - array[lo]);
+        final int midValue = array[mid];
+        if(lo>mid){
+            Log.d(TAG, "\n   第 "+interval+" 次:"+"\n 结果不存在，返回-1: ");
             return -1;
         }
-        interval++;
-        if (value > arr[middle]) {
+        if (target > midValue) {
             //向右递归
-            Log.d(TAG, "\n   第 "+interval+" 次:"+"\n 指针向 右 移: "+"\n left: "+left+" \n right: "+right+" \n middle: " +middle +" \n value: "+value);
-            return InsertSearch(arr, middle + 1, right,value);
-        } else if (value < arr[middle]) {
+            Log.d(TAG, "\n   第 "+interval+" 次:"+"\n 指针向 右 移: "+"\n left: "+lo+" \n right: "+hi+" \n middle: " +mid +" \n value: "+target);
+            return InsertSearch(array, mid + 1, hi,target);
+        } else if (target < midValue) {
             //向左递归
-            Log.d(TAG, "\n   第 "+interval+" 次:"+"\n 指针向 左 移: "+"\n left: "+left+" \n right: "+right+" \n middle: " +middle +" \n value: "+value);
-            return InsertSearch(arr, left,middle - 1,value);
+            Log.d(TAG, "\n   第 "+interval+" 次:"+"\n 指针向 左 移: "+"\n left: "+lo+" \n right: "+hi+" \n middle: " +mid +" \n value: "+target);
+            return InsertSearch(array, lo,mid - 1,target);
         } else {
-            Log.d(TAG, "\n   第 "+interval+" 次:"+"\n 指针找到结果，返回结果: "+middle);
-            return middle;
+            Log.d(TAG, "\n   第 "+interval+" 次:"+"\n 指针找到结果，返回结果: "+mid);
+            return mid;
         }
     }
 
